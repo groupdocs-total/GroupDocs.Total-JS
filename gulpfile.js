f82f57f134e87fe536b1267c6cd6b65b2cbd0a92
Pavel Egorov <egorovpasha@gmail.com>
2018-11-07 17:37:39 +0000

    added browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('css-watch', function(){
+    return gulp.src("**/css/*.css")
+        .pipe(browserSync.stream());
+});
+
+gulp.task('js-watch', function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch(["**/css/*.css"],['css-watch']);
+    gulp.watch(["**/js/*.js"], ['js-watch']);
+});

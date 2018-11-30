7073182a114167c1c77dbce887e2016ed995c074
pavel.teplitsky <pavel.teplitsky@groupdocs.com>
2018-11-30 10:22:24 +0200

    Updated versions

--- packages/annotation/js/annotation.js
+++ packages/annotation/js/annotation.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2018 Aspose Pty Ltd
  * Licensed under MIT.
  * @author Aspose Pty Ltd
- * @version 1.6.0
+ * @version 1.7.0
  */
 
 /*
--- packages/annotation/js/drawTextAnnotation.js
+++ packages/annotation/js/drawTextAnnotation.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2018 Aspose Pty Ltd
  * Licensed under MIT.
  * @author Aspose Pty Ltd
- * @version 1.2.0
+ * @version 1.3.0
  */
  
 //////////////////////////////////////////////////

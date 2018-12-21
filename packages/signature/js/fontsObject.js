14f95d5ffce169a67428fc959de604b708661c3c
pavel.teplitsky <pavel.teplitsky@groupdocs.com>
2018-12-21 16:04:39 +0200

    Added bcPicker.css and fontsObject.js

---
+++ packages/signature/js/fontsObject.js
@@ -0,0 +1,47 @@
+/**
+ * groupdocs.signature Plugin
+ * Copyright (c) 2018 Aspose Pty Ltd
+ * Licensed under MIT.
+ * @author Aspose Pty Ltd
+ * @version 1.0.0
+ */
+
+(function( $ ) {
+
+	/**
+	* Create private variables.
+	**/
+	var cssFonts = [
+		"Arial",
+		"Helvetica",
+		"Gill Sans",
+		"Lucida",
+		"Helvetica Narrow",
+		"Times",
+		"Times New Roman",
+		"Palatino",
+		"Bookman",
+		"New Century Schoolbook",
+		"Andale Mono",
+		"Courier New",
+		"Courier",
+		"Lucidatypewriter",
+		"Fixed",
+		"Comic Sans", 
+		"Comic Sans MS",
+		"Zapf Chancery",
+		"Coronetscript",
+		"Florence",
+		"Parkavenue",
+		"Impact",
+		"Arnoldboecklin",
+		"Oldtown",
+		"Blippo",
+		"Brushstroke",
+		"fantasy"
+	];	
+	
+	$.fn.cssFonts = function() {
+        return cssFonts;
+	}
+})(jQuery);

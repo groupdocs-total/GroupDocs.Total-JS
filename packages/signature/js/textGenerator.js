0a219bcd717671dca4589442cfcfc2325bf6e9f5
Elena Kolesnikova <koleslena@yandex.ru>
2019-01-09 22:53:49 +0600

    fixed text signature

--- packages/signature/js/textGenerator.js
+++ packages/signature/js/textGenerator.js
@@ -25,101 +25,118 @@
         '<div id="' + paramValues.fontColor + '" class="gd-text-color-picker"></div>'];
 	var properties = {};
 	
-	$.fn.textGenerator = function(parentName) {
-		getFonts();
-        $(this).append($.fn.textGenerator.baseHtml());
-
-        paramValues.parentName = parentName;
-
-        $('#' + parentName).find("#" + paramValues.fontColor).bcPicker();
-
-		$('#' + parentName).on("change", "#" +  + paramValues.font, function(e) {
-            var val = $(this).val();
-            $('#' + parentName).find('#' + paramValues.text).css("font-family", val);
-            properties.font = val;
-		});
-		
-		$('#' + parentName).on("change", "#" +  + paramValues.fontSize, function(e) {
-            var val = $(this).val();
-            $('#' + parentName).find('#' + paramValues.text).css("font-size", val);
-            properties.fontSize = val;
-		});
-		
-		$('#' + parentName).on(userMouseClick, "#" +  + paramValues.bold, function(e) {
-			e.preventDefault();
-			e.stopImmediatePropagation();
-			$('#' + parentName).find('#' + paramValues.bold).toggleClass("active");
-			if($('#' + parentName).find('#' + paramValues.text).css("font-weight") == "400") {
-				$('#' + parentName).find('#' + paramValues.text).css("font-weight", "bold");
-                properties.bold = true;
-			} else {
-				$('#' + parentName).find('#' + paramValues.text).css("font-weight", "unset");
-                properties.bold = false;
-            }
-		});
-		
-		$('#' + parentName).on(userMouseClick, "#" +  + paramValues.italic, function(e) {
-			e.preventDefault();
-			e.stopImmediatePropagation();
-			$('#' + parentName).find('#' + paramValues.italic).toggleClass("active");
-			if($('#' + parentName).find('#' + paramValues.text).css("font-style") != "italic") {
-				$('#' + parentName).find('#' + paramValues.text).css("font-style", "italic");
-                properties.italic = true;
-			} else {
-				$('#' + parentName).find('#' + paramValues.text).css("font-style", "unset");
-                properties.italic = false;
-			}	
-		});
-		
-		$('#' + parentName).on(userMouseClick, "#" +  + paramValues.underline, function(e) {
-			e.preventDefault();
-			e.stopImmediatePropagation();
-			$('#' + parentName).find('#' + paramValues.underline).toggleClass("active");
-			if($('#' + parentName).find('#' + paramValues.text).css("text-decoration").indexOf("underline") == -1) {
-				$('#' + parentName).find('#' + paramValues.text).css("text-decoration", "underline");
-                properties.underline = true;
-			} else {
-				$('#' + parentName).find('#' + paramValues.text).css("text-decoration", "unset");
-                properties.underline = false;
-			}	
-		});
-		
-		$('#' + parentName).on(userMouseClick, ".bcPicker-color", function(e) {
-            var css = $(this).css("background-color");
-            $('#' + parentName).find('#' + paramValues.text).css("color", css);
-            properties.fontColor = css;
-		});
-		
-		$('#' + parentName).on(userMouseClick, ".fa-arrow-up", function(e) {
-			e.preventDefault();
-			e.stopImmediatePropagation();
-			if($(this).hasClass("down")){
-				$('#' + parentName).find(".gd-text-menu").css("top", "unset");
-			} else {
-				$('#' + parentName).find(".gd-text-menu").css("top", "100px");
-			}
-			$(this).toggleClass("down");
-		});
-		initProps();
-	};
+	$.fn.textGenerator = function() {
+    };
 
 	$.extend(true, $.fn.textGenerator, {
-	    init : function (parent, props) {
+	    create: function(parentName) {
+	        properties = {};
+            if (parentName && parentName.endsWith('0')) {
+                getFonts();
+            }
+
+            paramValues.parentName = parentName;
+            $('#' + paramValues.parentName).find('.gd-draw-text').append(baseHtml());
+
+            $('#' + parentName).find("#" + paramValues.fontColor).bcPicker();
+
+            $('#' + parentName).on("change", "#" + paramValues.font, function(e) {
+                var val = $(this).val();
+                $('#' + parentName).find('#' + paramValues.text).css("font-family", val);
+                properties.font = val;
+            });
+
+            $('#' + parentName).on("change", "#" + paramValues.fontSize, function(e) {
+                var val = $(this).val();
+                $('#' + parentName).find('#' + paramValues.text).css("font-size", val);
+                properties.fontSize = val;
+            });
+
+            $('#' + parentName).on(userMouseClick, "#" + paramValues.bold, function(e) {
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                $('#' + parentName).find('#' + paramValues.bold).toggleClass("active");
+                if($('#' + parentName).find('#' + paramValues.text).css("font-weight") == "400") {
+                    $('#' + parentName).find('#' + paramValues.text).css("font-weight", "bold");
+                    properties.bold = true;
+                } else {
+                    $('#' + parentName).find('#' + paramValues.text).css("font-weight", "unset");
+                    properties.bold = false;
+                }
+            });
+
+            $('#' + parentName).on(userMouseClick, "#" + paramValues.italic, function(e) {
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                $('#' + parentName).find('#' + paramValues.italic).toggleClass("active");
+                if($('#' + parentName).find('#' + paramValues.text).css("font-style") != "italic") {
+                    $('#' + parentName).find('#' + paramValues.text).css("font-style", "italic");
+                    properties.italic = true;
+                } else {
+                    $('#' + parentName).find('#' + paramValues.text).css("font-style", "unset");
+                    properties.italic = false;
+                }
+            });
+
+            $('#' + parentName).on(userMouseClick, "#" + paramValues.underline, function(e) {
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                $('#' + parentName).find('#' + paramValues.underline).toggleClass("active");
+                if($('#' + parentName).find('#' + paramValues.text).css("text-decoration").indexOf("underline") == -1) {
+                    $('#' + parentName).find('#' + paramValues.text).css("text-decoration", "underline");
+                    properties.underline = true;
+                } else {
+                    $('#' + parentName).find('#' + paramValues.text).css("text-decoration", "unset");
+                    properties.underline = false;
+                }
+            });
+
+            $('#' + parentName).on(userMouseClick, ".bcPicker-color", function(e) {
+                var css = $(this).css("background-color");
+                $('#' + parentName).find('#' + paramValues.text).css("color", css);
+                properties.fontColor = css;
+            });
+
+            $('#' + parentName).on(userMouseClick, ".fa-arrow-up", function(e) {
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                if($(this).hasClass("down")){
+                    $('#' + parentName).find(".gd-text-menu").css("top", "unset");
+                } else {
+                    $('#' + parentName).find(".gd-text-menu").css("top", "100px");
+                }
+                $(this).toggleClass("down");
+            });
+            initProps();
+            initTextCss();
+        },
+
+	    init : function (parent, props, text, imageGuid) {
 	        if (! parent) {
 	            return;
             }
             paramValues.parentName = parent;
-	        if (props) {
+            if (props) {
                 properties = props;
-                $('#' + paramValues.parentName).find('#' + paramValues.text).css("color", props.fontColor);
-                $('#' + paramValues.parentName).find('#' + paramValues.text).val(props.text);
-                $('#' + paramValues.parentName).find('#' + paramValues.text).css("text-decoration", props.underline ? "underline" : "unset");
-                $('#' + paramValues.parentName).find('#' + paramValues.text).css("font-style", props.italic ? "italic" : "unset");
-                $('#' + paramValues.parentName).find('#' + paramValues.text).css("font-weight", props.bold ? "bold" : "unset");
-                $('#' + paramValues.parentName).find('#' + paramValues.text).css("font-size", props.fontSize);
-                $('#' + paramValues.parentName).find('#' + paramValues.text).css("font-family", props.font);
+
+                $('#' + paramValues.parentName).find("#" + paramValues.fontColor).find('.bcPicker-picker').css("background-color", props.fontColor);
+                if (props.underline) {
+                    $('#' + paramValues.parentName).find('#' + paramValues.underline).toggleClass("active");
+                }
+                if (props.italic) {
+                    $('#' + paramValues.parentName).find('#' + paramValues.italic).toggleClass("active");
+                }
+                if (props.bold) {
+                    $('#' + paramValues.parentName).find('#' + paramValues.bold).toggleClass("active");
+                }
+                $('#' + paramValues.parentName).find("#" + paramValues.fontSize).val(props.fontSize);
+                $('#' + paramValues.parentName).find("#" + paramValues.font).val(props.font);
+
+                initTextCss();
             } else {
                 initProps();
+                properties.text = text;
+                properties.imageGuid = imageGuid;
             }
         },
 
@@ -130,12 +147,6 @@
             properties.height = text.height();
 			return properties;
 		},
-
-        baseHtml : function() {
-            var html = '<textarea id="' + paramValues.text + '" class="gd-text">' +
-                '</textarea>';
-            return html;
-        },
 
 		getMenu : function () {
             var menuHtml = '<div class="gd-text-menu">';
@@ -153,16 +164,35 @@
         }
 	});
 
+    function baseHtml() {
+        var html = '<textarea id="' + paramValues.text + '" class="gd-text">' +
+            '</textarea>';
+        return html;
+    };
+
 	function initProps() {
         var text = $('#' + paramValues.parentName).find('#' + paramValues.text);
-        properties.underline = text.css("text-decoration").indexOf("underline") != -1;
-        properties.italic = text.css("font-style") == "italic";
-        properties.bold = text.css("font-weight") != "400";
         properties.width = text.width();
         properties.height = text.height();
-        properties.fontColor = $('#' + paramValues.parentName).find("#" + paramValues.fontColor).css("background-color");
+
+        properties.underline = $('#' + paramValues.parentName).find('#' + paramValues.underline)[0].className.indexOf("active") > 0;
+        properties.italic = $('#' + paramValues.parentName).find('#' + paramValues.italic)[0].className.indexOf("active") > 0;
+        properties.bold = $('#' + paramValues.parentName).find('#' + paramValues.bold)[0].className.indexOf("active") > 0;
+        properties.fontColor = $('#' + paramValues.parentName).find("#" + paramValues.fontColor).find('.bcPicker-picker').css("background-color");
         properties.font = $('#' + paramValues.parentName).find("#" + paramValues.font).val();
         properties.fontSize = $('#' + paramValues.parentName).find("#" + paramValues.fontSize).val();
+
+    }
+
+    function initTextCss() {
+        var textField = $('#' + paramValues.parentName).find('#' + paramValues.text);
+        textField.val(properties.text);
+        textField.css("text-decoration", properties.underline ? "underline" : "unset");
+        textField.css("font-style", properties.italic ? "italic" : "unset");
+        textField.css("font-weight", properties.bold ? "bold" : "unset");
+        textField.css("color", properties.fontColor);
+        textField.css("font-family", properties.font);
+        textField.css("font-size", properties.fontSize);
     }
 
     //////////////////////////////////////////////////
